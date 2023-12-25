0a637aae2a3552d96864cf966a8b820c6eaba1f7
Noam Saar <noamsaar11@gmail.com>
2023-12-25 16:36:52 +0200

    Refactor main.jsx and App.js

---
+++ src/root-cmp.jsx
@@ -0,0 +1,39 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import './assets/style/main.css'
+
+import { AppHeader } from './cmps/AppHeader'
+// import { AppFooter } from './cmps/AppFooter'
+
+import { HomePage } from './pages/HomePage'
+import { AboutUs } from './pages/AboutUs'
+import { ToyIndex } from './pages/ToyIndex.jsx'
+import { ToyDetails } from './pages/ToyDetails.jsx'
+// import { ToyEdit } from './pages/ToyEdit.jsx'
+import { store } from './store/store'
+
+
+export function App() {
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <section className="main-layout app">
+          <AppHeader />
+          <main>
+            <Routes>
+              <Route element={<HomePage />} path="/" />
+              <Route element={<AboutUs />} path="/about" />
+              <Route element={<ToyIndex />} path="/toy" />
+              <Route element={<ToyDetails />} path="/toy/:toyId" />
+              {/* <Route element={<ToyEdit />} path="/toy/edit" /> */}
+            </Routes>
+          </main>
+          {/* <AppFooter /> */}
+        </section>
+      </Router>
+    </Provider>
+  )
+}
+
+
