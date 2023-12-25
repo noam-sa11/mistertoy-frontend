8e3d88e9805a7cadbe5d50f2010b684b49ea1f4d
Noam Saar <noamsaar11@gmail.com>
2023-12-25 16:36:27 +0200

    Add UserMsg

---
+++ src/cmps/UserMsg.jsx
@@ -0,0 +1,32 @@
+import { setMsg } from "../store/actions/app.actions.js"
+
+import { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+
+export function UserMsg() {
+    const msg = useSelector((storeState) => storeState.appModule.msg)
+
+    const timeoutIdRef = useRef()
+
+    useEffect(() => {
+        console.log('timeoutIdRef.current', timeoutIdRef.current)
+        if (!msg) return
+        if (timeoutIdRef.current) {
+            clearTimeout(timeoutIdRef.current)
+            timeoutIdRef.current = null
+        }
+        timeoutIdRef.current = setTimeout(closeMsg, 3000)
+    }, [msg])
+
+    function closeMsg() {
+        setMsg(null)
+    }
+
+    if (!msg) return <span></span>
+    return (
+        <section className={`user-msg ${msg.type}`}>
+            <button onClick={closeMsg}>x</button>
+            <p>{msg.txt}</p>
+        </section>
+    )
+}
