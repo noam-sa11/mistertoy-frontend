dfd2f14848f946cab4fed8965ce2f2ebc2c4345d
Noam Saar <noamsaar11@gmail.com>
2023-12-25 16:35:53 +0200

    Add HomePage and About

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from "react-redux"
+
+export function HomePage() {
+    const dispatch = useDispatch()
+    const count = useSelector(storeState => storeState.userModule.count)
+
+    function changeCount(diff) {
+        console.log('Changing count by:', diff)
+        dispatch({ type: 'CHANGE_BY', diff })
+    }
+
+    return (
+        <section>
+            <h2>
+                Count {count}
+                <button onClick={() => {
+                    changeCount(1)
+                }}>+</button>
+                <button onClick={() => {
+                    changeCount(10)
+                }}>+10</button>
+            </h2 >
+        </section >
+    )
+}
