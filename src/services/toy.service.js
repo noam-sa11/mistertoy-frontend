d37113d46a6c5f92ef44ed1cc4694c70fe83a10b
Noam Saar <noamsaar11@gmail.com>
2023-12-28 15:19:31 +0200

    Refactor toy.actions to new async/error syntax

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -18,26 +18,63 @@ export const toyService = {
 
 // const labels = ["On wheels", "Box game", "Art", "Baby", "Doll", "Puzzle", "Outdoor", "Battery Powered"]
 
-function query(filterBy = {}) {
-    return httpService.get(BASE_URL, filterBy)
-}
-
-function getToyById(toyId) {
-    return httpService.get(BASE_URL + toyId)
-}
-
-function save(toy) {
-    if (toy._id) {
-        return httpService.put(BASE_URL + toy._id, toy)
-    } else {
-        toy.createdAt = Date.now()
-        return httpService.post(BASE_URL, toy)
+async function query(filterBy = {}) {
+    try {
+        return await httpService.get(BASE_URL, filterBy)
+    } catch (error) {
+        console.error('Error during toy query:', error)
+        throw error
     }
 }
+// function query(filterBy = {}) {
+//     return httpService.get(BASE_URL, filterBy)
+// }
 
-function removeToy(toyId) {
-    return httpService.delete(BASE_URL + toyId)
+async function getToyById(toyId) {
+    try {
+        return await httpService.get(BASE_URL + toyId)
+    } catch (error) {
+        console.error('Error getting toy by ID:', error)
+        throw error
+    }
 }
+// function getToyById(toyId) {
+//     return httpService.get(BASE_URL + toyId)
+// }
+
+async function save(toy) {
+    try {
+        if (toy._id) {
+            return await httpService.put(BASE_URL + toy._id, toy)
+        } else {
+            toy.createdAt = Date.now()
+            return await httpService.post(BASE_URL, toy)
+        }
+    } catch (error) {
+        console.error('Error during toy save:', error)
+        throw error
+    }
+}
+// function save(toy) {
+//     if (toy._id) {
+//         return httpService.put(BASE_URL + toy._id, toy)
+//     } else {
+//         toy.createdAt = Date.now()
+//         return httpService.post(BASE_URL, toy)
+//     }
+// }
+
+async function removeToy(toyId) {
+    try {
+        return await httpService.delete(BASE_URL + toyId)
+    } catch (error) {
+        console.error('Error during toy removal:', error)
+        throw error
+    }
+}
+// function removeToy(toyId) {
+//     return httpService.delete(BASE_URL + toyId)
+// }
 
 function getDefaultFilter() {
     return { name: '', maxPrice: '', inStock: 'all', labels: [] }
